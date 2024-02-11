c0331f90a04982a2387981a1e12e8c67f000fe65
XoDus <xodusgd@gmail.com>
2024-02-10 23:15:58 -0500

    Functionality including parsing, and object-oriented behavior present

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import cv2
 from PIL import Image
 import pytesseract
 import re
+import CamData
 
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
@@ -9,10 +10,9 @@ video_path = 'vid/20240130162559_000927.MP4'
 cap = cv2.VideoCapture(video_path)
 
 roi_x, roi_y, roi_width, roi_height = 130, 1390, 390, 30
+secondDelay = 5
 
-coords_array = []
-coords_dict = {}
-
+CamDataArray = []
 
 def extract_text(video_frame):
     roi = video_frame[roi_y:roi_y+roi_height, roi_x:roi_x+roi_width]
@@ -25,20 +25,23 @@ def extract_text(video_frame):
     return alphanumeric_text.strip().replace('/', '')
 
 
+secondCounter = 0
 while cap.isOpened():
 
-    cap.set(cv2.CAP_PROP_POS_MSEC, cap.get(cv2.CAP_PROP_POS_MSEC) + 1000)
+    cap.set(cv2.CAP_PROP_POS_MSEC, cap.get(cv2.CAP_PROP_POS_MSEC) + 1000 * secondDelay)
 
     ret, frame = cap.read()
+    secondCounter += secondDelay
 
     if not ret:
         break
 
     extracted_text = extract_text(frame)
-    coords_array.append(extracted_text)
+    extracted_lat, extracted_lon = extracted_text.split(sep=" ")
+    CamDataArray.append(CamData.CamData(secondCounter, extracted_lat, extracted_lon))
 
 cap.release()
 cv2.destroyAllWindows()
 
-for i, coords in enumerate(coords_array):
-    print(f"Second {i+1}: {coords}")
+for data in CamDataArray:
+    print(data.printData())
